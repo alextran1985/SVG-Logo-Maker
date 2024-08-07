b1113b42b0576234ed8952420d03c2cedb7960b8
Alex Tran <Lockey5891@aol.com>
2024-08-07 15:46:23 -0700

    edited code to the index.js

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 As a freelance web developer I want to generate a simple logo for my projects so that I don't have to pay a graphic designer.
 
+## Acceptance Criteria
+
 GIVEN a command-line application that accepts user input
 WHEN I am prompted for text
 THEN I can enter up to three characters
@@ -30,6 +32,8 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
+Please refer to video link for installation instructions.
+
 ## Usage
 
 ![ScreenShot](assets/images/screenshot.png)
@@ -46,10 +50,7 @@ Please refer to the GitHub for License info.
 
 If your project has a lot of features, list them here.
 
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+To test the application run npm test.
--- index.js
+++ index.js
@@ -1,48 +1,81 @@
-const inquirer = require ('inquirer');
-const {Circle, Triangle, Square} = require ('./lib/shapes');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const { Circle, Triangle, Square } = require("./lib/shapes");
+const fs = require("fs");
 
-class Svg{
-    constructor(){
-        this.textElement = ''
-        this.shapeElement = ''
-    }
-    render(){
-        return`<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">`
-    }
-    setTextElement(text,color){
-        this.textElement = `<text x='150 y='125 font-size='70' text-anchor='min`
-    }
-    setShapeElement(shape){
-        this.shapElement = shape.render()
-    }
+class Svg {
+  constructor() {
+    this.textElement = "";
+    this.shapeElement = "";
+  }
+  render() {
+    return `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">`;
+  }
+  setTextElement(text, color) {
+    this.textElement = `<text x='150 y='125 font-size='70' text-anchor='min`;
+  }
+  setShapeElement(shape) {
+    this.shapElement = shape.render();
+  }
 }
 
 // Ask user questions
 const questions = [
-{
-    type: 'input',
-    name: 'text',
-    message: 'Text: Please choose 3 Letters:',
-}
-{
-    type: 'input',
-    name: 'text-color',
-    message: 'What color would you like your text to be?',
-}
-{
-    type: 'list',
-    name: 'shape',
-    message: 'What shape would you like',
-    choices: ['Circle','Triangle','Square'],
-}
-{
-    type: 'input',
-    name: 'shape-color',
-    message: 'What color would you like you shape to be',
-}
-
-
+  {
+    type: "input",
+    name: "text",
+    message: "Text: Please choose 3 Letters:",
+  },
+  {
+    type: "input",
+    name: "text-color",
+    message: "What color would you like your text to be?",
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "What shape would you like",
+    choices: ["Circle", "Triangle", "Square"],
+  },
+  {
+    type: "input",
+    name: "shape-color",
+    message: "What color would you like you shape to be",
+  },
 ];
+
+//Prompt the questions to the user
+inquirer.prompt(questions).then((answers) => {
+  let shape;
+
+  switch (answers.shape) {
+    case "Circle":
+      shape = new Circle();
+      break;
+    case "Triangle":
+      shape = new Triangle();
+      break;
+    case "Square":
+      shape = new Square();
+      break;
+  }
+  shape.setColor(answers.shapeColor);
+
+  const svgContent = `
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg width="200" height="250"  ">
+    ${shape.render()}
+    <text x='150'y='125' font-size='60' text-anchor='middle' fill="${
+      answers.textColor
+    }">${answers.text}</text>
+    </svg>`;
+
+  //make sure output exists
+  if (!fs.existsSync("output")) {
+    fs.mkdirSync("output");
+  }
+  //Write Svg to the file
+  fs.writeFileSync("output/examples.svg", svgContent);
+  //let user know the logo was a success
+  console.log("Svg logo was successfully create!");
+});
 
 
